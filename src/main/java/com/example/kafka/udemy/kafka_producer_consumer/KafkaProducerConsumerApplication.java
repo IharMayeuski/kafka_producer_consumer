d4746b12e492cdd2d2a18bd439e6942146386ae3
Igor <maevskij.i@gmail.com>
2021-04-29 23:36:40 +0300

    Initial commit

---
+++ src/main/java/com/example/kafka/udemy/kafka_producer_consumer/KafkaProducerConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.example.kafka.udemy.kafka_producer_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProducerConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaProducerConsumerApplication.class, args);
+    }
+}
---
+++ src/main/java/com/example/kafka/udemy/kafka_producer_consumer/SimpleModel.java
@@ -0,0 +1,16 @@
+package com.example.kafka.udemy.kafka_producer_consumer;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class SimpleModel {
+    private String field1;
+    private String field2;
+}
